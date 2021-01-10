f3b0e1897ecc074b7b530fe37e86ca3c708b6d36
Edris Khorani <ek9g13@gmail.com>
2021-01-10 14:55:59 +0000

    Begin function building for model in notebook

--- hts_forecast/src/model.py
+++ hts_forecast/src/model.py
@@ -1,5 +1,12 @@
-def func1():
-    """
+# import relevant python libraries
+import pandas as pd
 
-    """
-    return 1
+sales = pd.read_csv('/Users/user/hts-forecast/volume/data/raw/sales_train_evaluation.csv')
+cal = pd.read_csv('/Users/user/hts-forecast/volume/data/raw/calendar.csv')
+
+def categorize(frame, category):
+    newframe = frame[frame.dept_id == category]
+    newframe2 = newframe.drop(['id', 'dept_id', 'cat_id', 'state_id'], axis = 'columns')
+    return newframe2
+
+categorize(sales, FOODS_3)
