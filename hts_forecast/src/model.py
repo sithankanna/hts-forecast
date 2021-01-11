d8e1bac1730de17c6faa92fd97970d3f4c6dc06c
Edris Khorani <ek9g13@gmail.com>
2021-01-11 20:37:30 +0000

    Create train-test-split/model/predict functions

--- hts_forecast/src/model.py
+++ hts_forecast/src/model.py
@@ -4,9 +4,9 @@ import pandas as pd
 sales = pd.read_csv('/Users/user/hts-forecast/volume/data/raw/sales_train_evaluation.csv')
 cal = pd.read_csv('/Users/user/hts-forecast/volume/data/raw/calendar.csv')
 
-def categorize(frame, category):
-    newframe = frame[frame.dept_id == category]
-    newframe2 = newframe.drop(['id', 'dept_id', 'cat_id', 'state_id'], axis = 'columns')
-    return newframe2
+#def categorize(frame, category):
+ #   newframe = frame[frame.dept_id == category]
+  #  newframe2 = newframe.drop(['id', 'dept_id', 'cat_id', 'state_id'], axis = 'columns')
+   # return newframe2
 
-categorize(sales, FOODS_3)
+#categorize(sales, FOODS_3)
